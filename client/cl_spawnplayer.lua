d17fcb118eae3720396182a3690f783e79de7566
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-03-24 14:45:03 +0000

    Update cl_spawnplayer.lua
    localized as much as possible added to the config for radar on foot and on vehicles/horses 
    add to the config to show or hide CORE UI for players

--- client/cl_spawnplayer.lua
+++ client/cl_spawnplayer.lua
@@ -2,14 +2,42 @@ local firstSpawn = true
 local active = false
 local playerHash = GetHashKey("PLAYER")
 
+local mapTypeOnMount = Config.mapTypeOnMount
+local playerCores = Config.playerCores
+local horseCores = Config.horseCores
+local mapTypeOnFoot = Config.mapTypeOnFoot
+
+
+
+
 function setPVP()
     NetworkSetFriendlyFireOption(true)
     SetRelationshipBetweenGroups(5, playerHash, playerHash)
 end
 
-RegisterNetEvent('vorp:initCharacter', function(coords, heading, isdead)
-    TeleportToCoords(coords.x+0.0, coords.y+0.0, coords.z+0.0, heading+0.0)
+--------------------------- UI RADAR SHOW OR HIDE ---------------------
+function UI()
+    local player = PlayerPedId()  
+    local playerOnMout = IsPedOnMount(player)
+    local playerOnVeh = IsPedInAnyVehicle(player)
+    if Config.HideUi then
+        TriggerEvent("vorp:showUi", false)
+    end
 
+    if not playerOnMout and not playerOnVeh then
+        SetMinimapType(mapTypeOnFoot)
+       
+    elseif  playerOnMout or playerOnVeh  then
+        SetMinimapType(mapTypeOnMount)
+      
+    end
+end
+----------------------------------------------------------------------
+
+RegisterNetEvent('vorp:initCharacter', function(coords, heading, isdead)
+    local player = PlayerPedId()
+    
+    TeleportToCoords(coords.x+0.0, coords.y+0.0, coords.z+0.0, heading+0.0)
     if isdead then
         if not Config.CombatLogDeath then 
             TriggerServerEvent("vorp:PlayerForceRespawn")
@@ -18,36 +46,39 @@ RegisterNetEvent('vorp:initCharacter', function(coords, heading, isdead)
         else
             Citizen.Wait(8000) --Don't think this is needed...
             TriggerEvent("vorp_inventory:CloseInv")
-            SetEntityHealth(PlayerPedId(),0,0)
+            SetEntityHealth(Player,0,0)
         end
     end
 end)
 
 RegisterNetEvent('vorp:SelectedCharacter', function()
-    local playerPedId = PlayerPedId()
-    local playerCoords, playerHeading = GetEntityCoords(playerPedId, true, true), GetEntityHeading(playerPedId)
-
+    local player = PlayerPedId()
+    local playerCoords, playerHeading = GetEntityCoords(player, true, true), GetEntityHeading(player)
+    local playerId = PlayerId()
     TriggerServerEvent("vorp:saveLastCoords", playerCoords, playerHeading)
         
     firstSpawn = false
 
     SetMinimapHideFow(true)
 
-    if Config["ActiveEagleEye"] then
-        Citizen.InvokeNative(0xA63FCAD3A6FEC6D2, PlayerId(), true)
+    if Config.ActiveEagleEye then
+        Citizen.InvokeNative(0xA63FCAD3A6FEC6D2, playerId, true)
     end
 
-    if Config["ActiveDeadEye"] then
-        Citizen.InvokeNative(0x95EE1DEE1DCD9070, PlayerId(), true)
+    if Config.ActiveDeadEye then
+        Citizen.InvokeNative(0x95EE1DEE1DCD9070, playerId, true)
     end
 
     setPVP()
+  
 end)
 
 AddEventHandler('playerSpawned', function(spawnInfo)
     Citizen.Wait(4000)
     TriggerServerEvent("vorp:playerSpawn")
     TriggerServerEvent("vorp:chatSuggestion") --- chat add suggestion trigger 
+
+
 end)
 
 Citizen.CreateThread(function()
@@ -58,25 +89,29 @@ Citizen.CreateThread(function()
     end
 end)
 
+
+
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        local pped = PlayerPedId()
+        local player = PlayerPedId()
         local count = 0
+        local playerOnMout = IsPedOnMount(player)
+        local playerOnVeh = IsPedInAnyVehicle(player,false)
 
-        if IsControlPressed(0, 0xCEFD9220) then
-            SetRelationshipBetweenGroups(1, playerHash, playerHash)
+        if IsControlPressed(0, 0xCEFD9220) then -- E
+            SetRelationshipBetweenGroups(1, playerHash, playerHash) -- 1 friendly 
             active = true
             Citizen.Wait(4000)
         end
 
-        if not IsPedOnMount(pped) and not IsPedInAnyVehicle(pped, false) and active then
-            SetRelationshipBetweenGroups(5, playerHash, playerHash)
-            active = false
-        elseif active and IsPedOnMount(pped) or IsPedInAnyVehicle(pped, false) then
-            if IsPedInAnyVehicle(pped, false) then
+        if not playerOnMout and not playerOnVeh and active then
+            SetRelationshipBetweenGroups(5, playerHash, playerHash) -- 5 hate 
+            active = false 
+        elseif active and playerOnMout or playerOnVeh  then
+            if playerOnVeh then 
                 --Nothing?
-            elseif GetPedInVehicleSeat(GetMount(pped), -1) == pped then
+            elseif GetPedInVehicleSeat(GetMount(player), -1) == player then
                 SetRelationshipBetweenGroups(5, playerHash, playerHash)
                 active = false
             end
@@ -84,21 +119,21 @@ Citizen.CreateThread(function()
     end
 end)
 
+
+
+
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(3000)
-        if Config["HideUi"] then
-     
-            TriggerEvent("vorp:showUi", false)
-        end
-        if Config["HideRadar"] then
-           DisplayRadar(false) 
-        end
+        UI()  
         if not firstSpawn then
-            local playerPedId = PlayerPedId()
-            local playerCoords, playerHeading = GetEntityCoords(playerPedId, true, true), GetEntityHeading(playerPedId)
-
+            local player = PlayerPedId()
+            local playerCoords, playerHeading = GetEntityCoords(player, true, true), GetEntityHeading(player)
             TriggerServerEvent("vorp:saveLastCoords", playerCoords, playerHeading)
         end
     end
 end)
+
+
+
+
