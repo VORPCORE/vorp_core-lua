a4477c2dcaa3a5877ac958879cb4803519019661
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-04-03 19:17:16 +0100

    config update
    added translation for commands  plus organization

--- config.lua
+++ config.lua
@@ -3,40 +3,51 @@
 ----------------------------------------------------------------------------
 
 Config = {
-  initGold =  0.0,
-  initMoney=  200.0,
-  initRol =  0.0,
-  initXp =  0,
-  initJob =  "unemployed",
+
+  ----------------------------------------------------------------------------
+  ----------------------- STARTING POINT -------------------------------------
+  initGold     =  0.0, 
+  initMoney    =  200.0,
+  initRol      =  0.0,
+  initXp       =  0,
+  initJob      =  "unemployed",
   initJobGrade =  0,
-  initGroup =  "user",
-  
-  Whitelist =  false,
+  initGroup    =  "user",
+  Whitelist     =  false, -- LEAVE TO FALSE IT'S NOT WORKING
   AllowWhitelistAutoUpdate =  false,
-  MaxCharacters =  5,
-  ActiveVoiceChat =  false,
-  KeySwapVoiceRange =  0x80F28E95,
+  MaxCharacters =  5, --MAX ALLOWED TO BE CREATED
+
+  ------------------------------------------------------------------------------
+  ------------------------------ WEBHOOKS --------------------------------------
+  --TODO
+  -----------------------------------------------------------------------------
+  ---------------------------- VOICE ------------------------------------------
+  ActiveVoiceChat   =  false,
+  KeySwapVoiceRange =  0x80F28E95, --[L] KEY
   DefaultVoiceRange =  5.0,
-  VoiceRanges = {2.0, 5.0, 12.0},
+  VoiceRanges       = {2.0, 5.0, 12.0},
   
+  -----------------------------------------------------------------------------
+  ------------------------- SHOW OR HIDE UI's ---------------------------------
+
+  HideUi          = true, --show or hide the UI includes  gold cash ID and level bar  the cash gold ID are now being displayed in the inventory. you can disable this one if yo ulike it more
+  mapTypeOnFoot   = 1, -- 0 = Off(no radar), 1 = Regular 2 = Expanded  3 = Simple(compass), for on foot
+  mapTypeOnMount  = 1,-- 0 = Off(no radar), 1 = Regular 2 = Expanded  3 = Simple(compass), for on horse
+  enableTypeRadar = false, --- if true the above will work, if false players can choose their radar type in the game settings.
+
+  ------------------------------------------------------------------------------
+  ------------------------------- RESPAWN --------------------------------------
+
+  RespawnTime =  10, --seconds
+  RespawnKey =  0xDFF812F9, --[E] KEY
+  RespawnTitleFont =  1, -- for the draw text message
+  RespawnSubTitleFont =  1, -- for the draw text message sub title font
   CombatLogDeath = true, -- people who combat log now spawn in dead rather than force spawned 
   ShowUiDeath = true, -- show or hide the UI if player is dead 
   camDeath = false, -- enable or disable the camera death function
-
-  ------------------------- SHOW OR HIDE UI's --------------------------------
-  HideUi = true, --show or hide the UI includes  gold cash ID and level bar  the cash gold ID are now being displayed in the inventory. you can disable this one if yo ulike it more
-  mapTypeOnFoot = 3, -- 0 = Off(no radar), 1 = Regular 2 = Expanded  3 = Simple(compass), for on foot
-  mapTypeOnMount = 1,-- 0 = Off(no radar), 1 = Regular 2 = Expanded  3 = Simple(compass), for on horse
-  enableTypeRadar = true, --- if true the above will work, if false players can choose their radar type in the game settings.
-  -----------------------------------------------------------------------------------
-  RespawnTime =  10, -- 25 seconds
-  RespawnKey =  0xDFF812F9,
-  RespawnTitleFont =  1, -- for the draw text message
-  RespawnSubTitleFont =  1, -- for the draw text message sub title font
-  
   sprite = true, --- enable text with sprite or disable
   spriteGrey = false, -- if set to false will enable RED sprite true will be grey
- ---------------------------- RESPAWN COORDS ------------------------------------
+
   hospital =  {
     Valentine = {
       name = "Valentine",
@@ -74,7 +85,7 @@ Config = {
       h = 321.76
     },
   },
- ----------------------------------------------------------------------------- 
+  ----------------------------------------------------------------------------- 
   HeadId =  false,
   HeadIdDistance =  15,
   ModeKey =  true,
@@ -82,44 +93,56 @@ Config = {
   ActiveEagleEye =  true,
   ActiveDeadEye =  false,
 
+  ----------------------------------------------------------------------------
+  --------------------------- COMMAND PERMISSION -----------------------------
+  Group = {
+    Admin = "admin", --- group for all commands 
+    Mod = "modertor", --- second group for all commands 
+  },
+
+  --------------------------------------------------------------------------------------
+  -----------------------------BUILT IN RICH PRESENCE DISCORD --------------------------
+  maxplayers = 128, -- change to the number of players that can get in to your server
+  appid = nil, -- Application ID (Replace this with you own)
+  biglogo = "synred", -- image assets name for the "large" icon.
+  biglogodesc = " Redm Server Connect: ",-- text when hover over image 
+  smalllogo = "smallboy",  -- image assets name for the "small" icon.(OPTIONAL)
+  smalllogodesc = "Join us for a good time", -- text when hover over image 
+  discordlink = "https://discord.gg/", -- discord link 
+  shownameandid = true, --show player steam name and id 
+
+
+  ---------------------------------------------------------------------------------------
+  ------------------------------- TRANSLATE ---------------------------------------------
+  Langs = {
+    IsConnected        = "🚫 Duplicated account connected (steam | rockstar)",
+    NoSteam            = "🚫 You have to have Steam open, please open Steam & restart RedM",
+    NoInWhitelist      = "🚫 You are not in the Whitelist",
+    NoPermissions      = "You don't have enough permissions",
+    CheckingIdentifier = "Checking Identifiers",
+    LoadingUser        = "Loading User",
+    BannedUser         = "You Are Banned",
+    TitleOnDead        = "Do /alertdoctor in chat to request doctors aid",
+    SubTitleOnDead     = "You can respawn in %s seconds",
+    RespawnIn          = "You can respawn in",
+    SecondsMove        = "seconds",
+    YouAreCarried      = "You are being carried by a person",
+    VoiceRangeChanged  = "Voice chat range changed to %s meters",
+    promptLabel        = "Respawn",  -- prompt label.   
+    prompt             = "Press",  -- prompt group label 
+    wayPoint           = "VORP: You need to set a waypoint first!",
+    mustBeSeated       = "VORP: You must be in the driver's seat!",
+    wagonInFront       = "VORP: You must be seated or near a wagon to delete it!",
+    cantCarry          = "VORP: Can't carry more weapons!",
+  },
+
+
+
 }
----------------------------------------------------------------------------------------
-
---------------------------- COMMAND PERMISSION GROUP ----------------------------------
-Config.Group = {
-  Admin = "admin", --- group for all commands including whitelist 
-  Mod = "Modertor", --- second group for whitelist
-}
----------------------------------------------------------------------------------------
-
-------------------------------- TRANSLATE ---------------------------------------------
-Config.Langs = {
-  IsConnected = "🚫 Duplicated account connected (steam | rockstar)",
-  NoSteam = "🚫 You have to have Steam open, please open Steam & restart RedM",
-  NoInWhitelist = "🚫 You are not in the Whitelist",
-  NoPermissions = "You don't have enough permissions",
-  CheckingIdentifier = "Checking Identifiers",
-  LoadingUser = "Loading User",
-  BannedUser = "You Are Banned",
-  TitleOnDead = "/alertdoctor in chat to request doctors aid",
-  SubTitleOnDead = "You can respawn in %s seconds",
-  RespawnIn = "You can respawn in  ~e~",
-  SecondsMove = "~q~ seconds",
-  YouAreCarried = "You are being carried by a person",
-  VoiceRangeChanged = "Voice chat range changed to %s meters",
-  promptLabel = "Respawn",  -- prompt label.   
-  prompt = "Press",  -- prompt group label 
-}
---------------------------------------------------------------------------------------
 
 
------------------------------BUILT IN RICH PRESENCE DISCORD --------------------------
-Config.maxplayers = 128
-Config.appid = nil -- Application ID (Replace this with you own)
-Config.biglogo = "synred" -- image assets name for the "large" icon.
-Config.biglogodesc = " Redm Server Connect: " -- text when hover over image 
-Config.smalllogo = "smallboy"  -- image assets name for the "small" icon.(OPTIONAL)
-Config.smalllogodesc = "Join us for a good time" -- text when hover over image 
-Config.discordlink = "https://discord.gg/" -- discord link 
-Config.shownameandid = true --show player steam name and id 
---------------------------------------------------------------------------------------
+
+
+
+
+
