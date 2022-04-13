19a75d6507eaaa0a3346c687f8210eb2ad0596d8
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-04-13 18:47:31 +0100

    clean up fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,54 +10,18 @@ shared_scripts {
 
 client_scripts {
   'client/Notifications.js',
-  'client/cl_adminactions.lua',
-  'client/cl_callback.lua',
-  'client/cl_clientapi.lua',
-  'client/cl_controls.lua',
-  'client/cl_discrichpresence.lua',
-  'client/cl_idheads.lua',
-  'client/cl_instance.lua',
-  'client/cl_miscellanea.lua',
-  'client/cl_notifications.lua',
-  'client/cl_playeractions.lua',
-  'client/cl_respawnsystem.lua',
-  'client/cl_spawnplayer.lua',
-  'client/cl_uicore.lua',
-  'client/cl_voicechat.lua',
-  'client/cl_dataview.lua'
+  'client/cl_*.lua',
 }
 
 server_scripts {
-  'server/class/sv_character.lua',
-  'server/class/sv_user.lua',
-  'server/sv_apicontroller.lua',
-  'server/sv_callbacks.lua',
-  'server/sv_commands.lua',
-  'server/sv_discrichpresence.lua',
-  'server/sv_init.lua',
-  'server/sv_loadcarachter.lua',
-  'server/sv_loadusers.lua',
-  'server/sv_old_luapi.lua',
-  'server/sv_savecoordsdb.lua',
-  'server/sv_whitelist.lua',
+  'server/class/sv_*.lua',
+  'server/sv_*.lua',
 }
 
 server_exports {'vorpAPI'}
 
 files {
-  'ui/hud.html',
-  'ui/css/style.css',
-  'ui/js/progressbar.js',
-  'ui/js/progressbar.min.js',
-  'ui/js/progressbar.min.js.map',
-  'ui/fonts/HapnaSlabSerif-DemiBold.ttf',
-  'ui/fonts/RDRCatalogueBold-Bold.ttf',
-  'ui/fonts/RDRGothica-Regular.ttf',
-  'ui/fonts/RDRLino-Regular.ttf',
-  'ui/fonts/rdrlino-regular-webfont.woff',
-  'ui/fonts/rdrlino-regular-webfont.woff2',
-  'ui/fonts/Redemption.ttf',
-  'ui/icons/*.png',
+  'ui/**/*'
 }
 
 ui_page 'ui/hud.html'
