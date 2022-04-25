7ef09d1b46261dc26d9da3ea82b609f41b1a8d15
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-04-25 07:35:25 +0100

    Fix for error in coords.
    Fixed the error you get in console when someone relog.

--- server/sv_savecoordsdb.lua
+++ server/sv_savecoordsdb.lua
@@ -1,4 +1,4 @@
-local LastCoordsInCache = {}
+LastCoordsInCache = {}
 
 RegisterNetEvent('vorp:saveLastCoords', function(lastCoords, lastHeading)
     local source = source
@@ -7,8 +7,9 @@ RegisterNetEvent('vorp:saveLastCoords', function(lastCoords, lastHeading)
     LastCoordsInCache[source] = { lastCoords, lastHeading }
 
     local characterCoords = json.encode({ x = math.floor(lastCoords.x) + 0.0, y = math.floor(lastCoords.y) + 0.0, z = math.floor(lastCoords.z) + 0.0, heading = math.floor(lastHeading) + 0.0 })
-
-    _users[identifier].GetUsedCharacter().Coords(characterCoords)
+    if _users[identifier] then
+        _users[identifier].GetUsedCharacter().Coords(characterCoords)
+    end
 end)
 
 RegisterNetEvent('vorp:ImDead', function(isDead)
