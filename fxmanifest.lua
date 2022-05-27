02a693272e45ba3466b045ae24e1a345dc7a7022
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-27 19:25:53 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -27,7 +27,7 @@ files {
 ui_page 'ui/hud.html'
 
 --dont touch
-version '1.0'
+version '1.1'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3 '
 vorp_github 'https://github.com/VORPCORE/vorp-core-lua'
