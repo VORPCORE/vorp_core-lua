e91ded241fc1860c162442b0f986a102a65ed65f
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-01-18 16:24:43 +0000

    refactor client commands

--- client/cl_commands.lua
+++ client/cl_commands.lua
@@ -1,51 +1,61 @@
--------------------------------------------------------------------------------------------------------------
--------------------------------------------- HIDEUI ---------------------------------------------------------
+--============================================ PLAYER COMMANDS ==============================================--
+
+PlayersCommands = {
+    hideui = {
+        command = "hideui",
+        suggestion = "VORPcore command to HIDE all UI's from screen, nice to take screenshots."
+    },
+    clear = {
+        command = "cleartask", suggestion = "VORPcore command to use if you are stuck on an animation."
+    },
+    pvp = {
+        command = "pvp",
+        suggestion = "VORPcore command to TOGGLE pvp for your character."
+    }
+}
+
 local hideUI = true
 
-RegisterCommand("hideui", function()
+CreateThread(function()
+    for _, value in pairs(PlayersCommands) do
+        RegisterCommand(value.command, function(source, args)
+            if value.command == "hideui" then
+                if hideUI then
+                    ExecuteCommand("togglechat") -- hide chat
+                    DisplayRadar(false)
+                    TriggerEvent("syn_displayrange", false) -- hud voice
+                    TriggerEvent("vorp:showUi", false) -- vorpui
+                    hideUI = false
+                else
+                    ExecuteCommand("togglechat")
+                    DisplayRadar(true)
+                    TriggerEvent("syn_displayrange", true)
+                    TriggerEvent("vorp:showUi", true)
+                    hideUI = true
+                end
+            elseif value.command == "cleartask" then
+                local player = PlayerPedId()
+                ClearPedTasksImmediately(player)
+            elseif value.command == "pvp" then
+                if Config.PVPToggle then
+                    Pvp = not Pvp
+                    if Pvp then
+                        TriggerEvent("vorp:TipRight", Config.Langs.PVPNotifyOn, 4000)
+                    else
+                        TriggerEvent("vorp:TipRight", Config.Langs.PVPNotifyOff, 4000)
+                    end
+                end
+            end
+        end)
 
-
-    if hideUI then
-        ExecuteCommand("togglechat")
-        DisplayRadar(false)
-        TriggerEvent("syn_displayrange", false)
-        TriggerEvent("vorp:showUi", false)
-        hideUI = false
-    else
-        ExecuteCommand("togglechat")
-        DisplayRadar(true)
-        TriggerEvent("syn_displayrange", true)
-        TriggerEvent("vorp:showUi", true)
-        hideUI = true
-    end
-end, false)
-
-TriggerEvent("chat:addSuggestion", "/hideui", " VORPcore command to HIDE all UI's from screen, nice to take screenshots.")
-
--------------------------------------------------------------------------------------------------------------
--------------------------------------------- CLEAR PED TASKS ---------------------------------------------------------
-
-RegisterCommand('cleartask', function()
-    local player = PlayerPedId()
-
-    ClearPedTasksImmediately(player)
-
-end, false)
-
-TriggerEvent("chat:addSuggestion", "/cleartask", " VORPcore command to use if you are stuck on an animation.")
-
--------------------------------------------------------------------------------------------------------------
--------------------------------------------- PVP Toggle ---------------------------------------------------------
-if Config.PVPToggle then
-    RegisterCommand("pvp", function()
-        pvp = not pvp
-
-        if pvp then
-            TriggerEvent("vorp:TipRight", Config.Langs.PVPNotifyOn, 4000)
+        if Config.PVPToggle then
+            TriggerEvent("chat:addSuggestion", "/" .. value.command, value.suggestion)
         else
-            TriggerEvent("vorp:TipRight", Config.Langs.PVPNotifyOff, 4000)
+            if value.command ~= "pvp" then
+                TriggerEvent("chat:addSuggestion", "/" .. value.command, value.suggestion)
+            end
         end
-    end)
-    TriggerEvent("chat:addSuggestion", "/pvp", " VORPcore command to TOGGLE pvp for your character.")
-end
--------------------------------------------------------------------------------------------------------------
+    end
+end)
+
+--============================================================================================================--
