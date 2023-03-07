86503c01df233f7d40a428c866841d42fb6d7431
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-03-07 20:56:14 +0000

    routing buckets  little optimizations

--- server/sv_instance.lua
+++ server/sv_instance.lua
@@ -1,43 +1,26 @@
-
 Namedinstances = {}
 
 RegisterServerEvent("vorp_core:instanceplayers")
 AddEventHandler("vorp_core:instanceplayers", function(setName)
-
     local _source = source
-    local instanceSource = nil
+    local instanceSource = setName ~= 0 and setName or nil
 
-    if setName == 0 then
-        for k, v in pairs(Namedinstances) do
-            for k2, v2 in pairs(v.people) do
-                if v2 == _source then
-                    table.remove(v.people, k2)
-                end
-            end
-            if #v.people == 0 then
-                Namedinstances[k] = nil
+    for k, v in pairs(Namedinstances) do
+        for k2, v2 in pairs(v.people) do
+            if v2 == _source then
+                table.remove(v.people, k2)
+                break
             end
         end
-        instanceSource = setName
-
-    else
-        for k, v in pairs(Namedinstances) do
-            if v.name == setName then
-                instanceSource = k
-            end
-        end
-
-        if instanceSource == nil then
-            instanceSource = setName
-
-            while Namedinstances[instanceSource] and #Namedinstances[instanceSource] >= 1 do
-                instanceSource = setName
-                Wait(1)
-            end
+        if #v.people == 0 then
+            Namedinstances[k] = nil
         end
     end
 
-    if instanceSource ~= 0 then
+    if instanceSource ~= nil then
+        while Namedinstances[instanceSource] and #Namedinstances[instanceSource].people >= 1 do
+            Wait(1)
+        end
 
         if not Namedinstances[instanceSource] then
             Namedinstances[instanceSource] = {
@@ -47,14 +30,9 @@ AddEventHandler("vorp_core:instanceplayers", function(setName)
         end
 
         table.insert(Namedinstances[instanceSource].people, _source)
-
-
     end
 
-    SetPlayerRoutingBucket(
-        _source,
-        instanceSource
-    )
+    SetPlayerRoutingBucket(_source, instanceSource)
 end)
 
 -- credits to MrDankKetchup
