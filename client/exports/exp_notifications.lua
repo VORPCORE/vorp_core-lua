f0e23c92a0af87f88eedea8297b3d1d41f7fee99
local9 <apjcookie1@hotmail.com>
2023-08-17 17:19:58 +0100

    feat: allow notification to be referenced directly
    This will ease performance for notifications, and allow the developer
    to directly implement them using a reference. By allowing them
    to be called directly will mean less calls being added to the
    event queue.
    
    ```lua
    client_scripts {
      "@vorp_core/client/ref/vorp_notifications.lua"
    }
    ```

---
+++ client/exports/exp_notifications.lua
@@ -0,0 +1,60 @@
+--============================= NOTIFICATION EXPORTS ==========================================--
+
+exports("DisplayTip", function(text, duration)
+  VorpNotification:DisplayTip(tostring(text), tonumber(duration))
+end)
+
+exports("DisplayLeftNotification", function(title, subTitle, dict, icon, duration, color)
+  VorpNotification:NotifyLeft(tostring(title), tostring(subTitle), tostring(dict), tostring(icon), tonumber(duration),
+    (tostring(color) or "COLOR_WHITE"))
+end)
+
+exports("DisplayTopCenterNotification", function(text, location, duration)
+  VorpNotification:DisplayTopCenter(tostring(text), tostring(location), tonumber(duration))
+end)
+
+exports("DisplayRightTip", function(text, duration)
+  VorpNotification:DisplayRightTip(tostring(text), tonumber(duration))
+end)
+
+exports("DisplayObjective", function(text, duration)
+  VorpNotification:DisplayObjective(tostring(text), tonumber(duration))
+end)
+
+exports("ShowTopNotification", function(title, subtext, duration)
+  VorpNotification:ShowTopNotification(tostring(title), tostring(subtext), tonumber(duration))
+end)
+
+exports("ShowAdvancedRightNotification", function(_text, _dict, icon, text_color, duration, quality, showquality)
+  VorpNotification:ShowAdvancedRightNotification(tostring(_text), tostring(_dict), tostring(icon), tostring(text_color),
+    tonumber(duration), quality, showquality)
+end)
+
+exports("ShowBasicTopNotification", function(text, duration)
+  VorpNotification:ShowBasicTopNotification(tostring(text), tonumber(duration))
+end)
+
+exports("ShowSimpleCenterText", function(text, duration, text_color)
+  VorpNotification:ShowSimpleCenterText(tostring(text), tonumber(duration), tostring(text_color))
+end)
+
+exports("showBottomRight", function(text, duration)
+  VorpNotification:ShowBottomRight(tostring(text), tonumber(duration))
+end)
+
+exports("failmissioNotifY", function(title, subTitle, duration)
+  VorpNotification:ShowFailedMission(tostring(title), tostring(subTitle), tonumber(duration))
+end)
+
+exports("deadplayerNotifY", function(title, _audioRef, _audioName, duration)
+  VorpNotification:ShowDeadPlayer(tostring(title), tostring(_audioRef), tostring(_audioName), tonumber(duration))
+end)
+
+exports("updatemissioNotify", function(utitle, umsg, duration)
+  VorpNotification:ShowUpdateMission(tostring(utitle), tostring(umsg), tonumber(duration))
+end)
+
+exports("warningNotify", function(title, msg, _audioRef, _audioName, duration)
+  VorpNotification:ShowWarning(tostring(title), tostring(msg), tostring(_audioRef), tostring(_audioName),
+    tonumber(duration))
+end)
