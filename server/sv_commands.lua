d8ca40a16ce253749aa77cf11f2f8bb7de90afa0
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-10-16 00:41:03 +0100

    feat: inventory exports usage, and addWeapon new features

--- server/sv_commands.lua
+++ server/sv_commands.lua
@@ -188,10 +188,10 @@ function AddItems(data)
     local item = tostring(data.args[2])
     local count = tonumber(data.args[3])
 
-    local VORPInv = exports.vorp_inventory:vorp_inventoryApi()
-    local itemCheck = VORPInv.getDBItem(target, item)
-    local canCarry = VORPInv.canCarryItems(target, count)       --can carry inv space
-    local canCarry2 = VORPInv.canCarryItem(target, item, count) --cancarry item limit
+    local VORPInv = exports.vorp_inventory
+    local itemCheck = VORPInv:getItemDB(item)
+    local canCarry = VORPInv:canCarryItems(target, count)       --can carry inv space
+    local canCarry2 = VORPInv:canCarryItem(target, item, count) --cancarry item limit
 
     if not itemCheck then
         return print(item .. " < item dont exist in the database", 4000)
@@ -205,7 +205,7 @@ function AddItems(data)
         return VorpCore.NotifyObjective(data.source, Translation[Lang].Notify.cantcarry, 4000)
     end
 
-    VORPInv.addItem(target, item, count)
+    VORPInv:addItem(target, item, count)
     SendDiscordLogs(data.config.webhook, data, data.source, item, count)
     VorpCore.NotifyRightTip(target, string.format(Translation[Lang].Notify.AddItems, item, count), 4000)
 end
@@ -214,14 +214,17 @@ end
 function AddWeapons(data)
     local target = tonumber(data.args[1])
     local weaponHash = tostring(data.args[2])
-    local VORPInv = exports.vorp_inventory:vorp_inventoryApi()
+    local serial = tostring(data.args[3]) or nil
+    local label = tostring(data.args[4]) or nil
+    local desc = tostring(data.args[5]) or nil
 
-    VORPInv.canCarryWeapons(target, 1, function(result) --can carry weapons
+    exports.vorp_inventory:canCarryWeapons(target, 1, function(result) --can carry weapons
         local canCarry = result
         if not canCarry then
             return VorpCore.NotifyObjective(data.source, T.cantCarry, 4000)
         end
-        VORPInv.createWeapon(target, weaponHash)
+
+        exports.vorp_inventory:createWeapon(target, weaponHash, nil, nil, nil, serial, label, desc)
         SendDiscordLogs(data.config.webhook, data, data.source, weaponHash, "")
         VorpCore.NotifyRightTip(target, Translation[Lang].Notify.AddWeapons, 4000)
     end, weaponHash)
