158592cd52c1b522316f65c38ea0039cc932ff85
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-11-11 13:57:08 +0000

    chore(server): remove unused code/duplicated

--- server/sv_loadcharacter.lua
+++ server/sv_loadcharacter.lua
@@ -1,41 +1,9 @@
-local characters = {}
 VorpCore = {}
 
 TriggerEvent("getCore", function(core)
     VorpCore = core
 end)
 
-function LoadCharacter(identifier, character)
-    characters[identifier] = character
-end
-
-RegisterNetEvent('vorp:playerSpawn', function()
-    local source = source
-    local sid = GetSteamID(source)
-
-    if sid and not characters[sid] then
-        characters[sid] = Character(source, sid, nil, "user", "unemployed", nil, nil, nil, "{}") 
-
-        TriggerClientEvent("vorpcharacter:createPlayer", source)
-    else
-        local pos = json.decode(characters[sid].Coords())
-
-        if pos and pos.x then
-            TriggerClientEvent("vorp:initPlayer", source, vector3(pos["x"], pos["y"], pos["z"]), pos["heading"],
-                characters[sid].IsDead())
-        end
-
-        characters[sid].source = source
-
-        -- Send Nui Update UI all
-        characters[sid].updateCharUi()
-    end
-end)
-
-RegisterNetEvent('vorp:UpdateCharacter', function(steamId, firstname, lastname)
-    characters[steamId].Firstname(firstname)
-    characters[steamId].Lastname(lastname)
-end)
 --TX ADMIN HEAL EVENT
 AddEventHandler('txAdmin:events:healedPlayer', function(eventData)
     if GetInvokingResource() ~= "monitor" or type(eventData) ~= "table" or type(eventData.id) ~= "number" then
@@ -44,8 +12,8 @@ AddEventHandler('txAdmin:events:healedPlayer', function(eventData)
     local Player = eventData.id
     if Player ~= -1 then
         local identifier = GetSteamID(Player)
-        local xCharacter = _users[identifier].GetUsedCharacter()
-        if xCharacter and xCharacter.isdead then
+        local Character = _users[identifier].GetUsedCharacter()
+        if Character and Character.isdead then
             TriggerClientEvent("vorp:TipRight", Player, Translation[Lang].Notify.healself, 4000)
             TriggerClientEvent('vorp:resurrectPlayer', Player)
             TriggerClientEvent('vorp:heal', Player)
@@ -68,7 +36,8 @@ end)
 RegisterServerEvent('vorp:SaveDate')
 AddEventHandler('vorp:SaveDate', function()
     local _source = source
-    local Character = VorpCore.getUser(_source).getUsedCharacter
+    local identifier = GetSteamID(_source)
+    local Character = _users[identifier].GetUsedCharacter()
     local charid = Character.charIdentifier
     MySQL.update("UPDATE characters SET LastLogin =NOW() WHERE charidentifier =@charidentifier",
         { charidentifier = charid })
