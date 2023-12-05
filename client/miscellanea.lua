ba909b0ca7770ef004fe9d842bdb4717e207496f
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-12-05 23:05:46 +0000

    Update miscellanea.lua

--- client/miscellanea.lua
+++ client/miscellanea.lua
@@ -41,6 +41,8 @@ local function FillUpCores()
     local a3 = DataView.ArrayBuffer(12 * 8)
     Citizen.InvokeNative("0xCB5D11F9508A928D", 1, a2:Buffer(), a3:Buffer(), GetHashKey("UPGRADE_HEALTH_TANK_1"),
         1084182731, Config.maxHealth, 752097756)
+    local a2 = DataView.ArrayBuffer(12 * 8)
+    local a3 = DataView.ArrayBuffer(12 * 8)
     Citizen.InvokeNative("0xCB5D11F9508A928D", 1, a2:Buffer(), a3:Buffer(), GetHashKey("UPGRADE_STAMINA_TANK_1"),
         1084182731, Config.maxStamina, 752097756)
 end
