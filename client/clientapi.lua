898bce1426ea750021a62c25bd11df557b613ccf
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-12-06 01:08:11 +0000

    feat(server): add export GetCore and included new callbacks system to object
    In this patch we are moving to an export to call Core object along side introducing the call back system to the Object so only one export  is needed.
    
    This means `TriggerEvent("getCore")` is now deprecated , but will still maintain functionality and even have the new call back system in it

--- client/clientapi.lua
+++ client/clientapi.lua
@@ -1,81 +1,98 @@
-AddEventHandler('getCore', function(cb)
-    local corefunctions = {}
+local CoreFunctions = {}
 
-    corefunctions.RpcCall = function(name, callback, ...)
+CoreFunctions.RpcCall = function(name, callback, ...)
+    ClientRPC.Callback.TriggerAsync(name, callback, ...)
+end
+
+CoreFunctions.instancePlayers = function(set)
+    TriggerServerEvent("vorp_core:instanceplayers", set)
+end
+
+CoreFunctions.AddWebhook = function(title, webhook, description, color, name, logo, footerlogo, avatar)
+    TriggerServerEvent('vorp_core:addWebhook', title, webhook, description, color, name, logo, footerlogo, avatar)
+end
+
+CoreFunctions.NotifyTip = function(text, duration)
+    VorpNotification:NotifyTip(tostring(text), tonumber(duration))
+end
+
+CoreFunctions.NotifyLeft = function(title, subtitle, dict, icon, duration, color)
+    VorpNotification:NotifyLeft(tostring(title), tostring(subtitle), tostring(dict), tostring(icon),
+        tonumber(duration), tostring(color or "COLOR_WHITE"))
+end
+
+CoreFunctions.NotifyRightTip = function(text, duration)
+    VorpNotification:NotifyRightTip(tostring(text), tonumber(duration))
+end
+
+CoreFunctions.NotifyObjective = function(text, duration)
+    VorpNotification:NotifyObjective(tostring(text), tonumber(duration))
+end
+
+CoreFunctions.NotifyTop = function(text, location, duration)
+    VorpNotification:NotifyTop(tostring(text), tostring(location), tonumber(duration))
+end
+
+CoreFunctions.NotifySimpleTop = function(text, subtitle, duration)
+    VorpNotification:NotifySimpleTop(tostring(text), tostring(subtitle), tonumber(duration))
+end
+
+CoreFunctions.NotifyAvanced = function(text, dict, icon, text_color, duration, quality, showquality)
+    VorpNotification:NotifyAvanced(tostring(text), tostring(dict), tostring(icon), tostring(text_color),
+        tonumber(duration), quality, showquality)
+end
+
+CoreFunctions.NotifyBasicTop = function(text, duration)
+    VorpNotification:NotifyBasicTop(tostring(text), tonumber(duration))
+end
+
+CoreFunctions.NotifyCenter = function(text, duration, text_color)
+    VorpNotification:NotifyCenter(tostring(text), tonumber(duration), tostring(text_color))
+end
+
+CoreFunctions.NotifyBottomRight = function(text, duration)
+    VorpNotification:NotifyBottomRight(tostring(text), tonumber(duration))
+end
+
+CoreFunctions.NotifyFail = function(text, subtitle, duration)
+    VorpNotification:NotifyFail(tostring(text), tostring(subtitle), tonumber(duration))
+end
+
+CoreFunctions.NotifyDead = function(title, audioRef, audioName, duration)
+    VorpNotification:NotifyDead(tostring(title), tostring(audioRef), tostring(audioName), tonumber(duration))
+end
+
+CoreFunctions.NotifyUpdate = function(title, subtitle, duration)
+    VorpNotification:NotifyUpdate(tostring(title), tostring(subtitle), tonumber(duration))
+end
+
+CoreFunctions.NotifyWarning = function(title, msg, audioRef, audioName, duration)
+    VorpNotification:NotifyWarning(tostring(title), tostring(msg), tostring(audioRef), tostring(audioName),
+        tonumber(duration))
+end
+
+CoreFunctions.NotifyLeftRank = function(title, subtitle, dict, icon, duration, color)
+    VorpNotification:NotifyLeftRank(tostring(title), tostring(subtitle), tostring(dict), tostring(icon),
+        tonumber(duration), tostring(color or "COLOR_WHITE"))
+end
+
+CoreFunctions.Callback = {
+    Register = function(name, callback)
+        ClientRPC.Callback.Register(name, callback)
+    end,
+    TriggerAsync = function(name, callback, ...)
         ClientRPC.Callback.TriggerAsync(name, callback, ...)
+    end,
+    TriggerAwait = function(name, ...)
+        return ClientRPC.Callback.TriggerAwait(name, ...)
     end
+}
 
-    corefunctions.instancePlayers = function(set)
-        TriggerServerEvent("vorp_core:instanceplayers", set)
-    end
-
-    corefunctions.AddWebhook = function(title, webhook, description, color, name, logo, footerlogo, avatar)
-        TriggerServerEvent('vorp_core:addWebhook', title, webhook, description, color, name, logo, footerlogo, avatar)
-    end
-
-    -- NOTIFICATIONS
-    corefunctions.NotifyTip = function(text, duration)
-        VorpNotification:NotifyTip(tostring(text), tonumber(duration))
-    end
-
-    corefunctions.NotifyLeft = function(title, subtitle, dict, icon, duration, color)
-        VorpNotification:NotifyLeft(tostring(title), tostring(subtitle), tostring(dict), tostring(icon), tonumber(duration), tostring(color or "COLOR_WHITE"))
-    end
-
-    corefunctions.NotifyRightTip = function(text, duration)
-        VorpNotification:NotifyRightTip(tostring(text), tonumber(duration))
-    end
-
-    corefunctions.NotifyObjective = function(text, duration)
-        VorpNotification:NotifyObjective(tostring(text), tonumber(duration))
-    end
-
-    corefunctions.NotifyTop = function(text, location, duration)
-        VorpNotification:NotifyTop(tostring(text), tostring(location), tonumber(duration))
-    end
-
-    corefunctions.NotifySimpleTop = function(text, subtitle, duration)
-        VorpNotification:NotifySimpleTop(tostring(text), tostring(subtitle), tonumber(duration))
-    end
-
-    corefunctions.NotifyAvanced = function(text, dict, icon, text_color, duration, quality, showquality)
-        VorpNotification:NotifyAvanced(tostring(text), tostring(dict), tostring(icon), tostring(text_color), tonumber(duration), quality, showquality)
-    end
-
-    corefunctions.NotifyBasicTop = function(text, duration)
-        VorpNotification:NotifyBasicTop(tostring(text), tonumber(duration))
-    end
-
-    corefunctions.NotifyCenter = function(text, duration, text_color)
-        VorpNotification:NotifyCenter(tostring(text), tonumber(duration), tostring(text_color))
-    end
-
-    corefunctions.NotifyBottomRight = function(text, duration)
-        VorpNotification:NotifyBottomRight(tostring(text), tonumber(duration))
-    end
-
-    corefunctions.NotifyFail = function(text, subtitle, duration)
-        VorpNotification:NotifyFail(tostring(text), tostring(subtitle), tonumber(duration))
-    end
-
-    corefunctions.NotifyDead = function(title, audioRef, audioName, duration)
-        VorpNotification:NotifyDead(tostring(title), tostring(audioRef), tostring(audioName), tonumber(duration))
-    end
-
-    corefunctions.NotifyUpdate = function(title, subtitle, duration)
-        VorpNotification:NotifyUpdate(tostring(title), tostring(subtitle), tonumber(duration))
-    end
-
-    corefunctions.NotifyWarning = function(title, msg, audioRef, audioName, duration)
-        VorpNotification:NotifyWarning(tostring(title), tostring(msg), tostring(audioRef), tostring(audioName),
-            tonumber(duration))
-    end
-
-    corefunctions.NotifyLeftRank = function(title, subtitle, dict, icon, duration, color)
-        VorpNotification:NotifyLeftRank(tostring(title), tostring(subtitle), tostring(dict), tostring(icon), tonumber(duration), tostring(color or "COLOR_WHITE"))
-    end
-
-    cb(corefunctions)
+exports('GetCore', function()
+    return CoreFunctions
 end)
 
---==========================================================================================--
+---@deprecated
+AddEventHandler('getCore', function(cb)
+    return cb(CoreFunctions)
+end)
