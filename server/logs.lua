d79447bfaf4bf5b7d26fbc03fdfccd121fe6ab26
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-07 15:33:29 +0000

    fix:(server/webhooks) assure to check for nil or empty strings to assign default values

--- server/logs.lua
+++ server/logs.lua
@@ -7,16 +7,16 @@ AddEventHandler('vorp_core:addWebhook', function(title, webhook, description, co
     end, 'POST', json.encode({
         embeds = {
             {
-                ["color"] = color or Config.webhookColor,
+                ["color"] = (not color or color == "") and Config.webhookColor or color,
                 ["author"] = {
-                    ["name"] = name or Config.name,
-                    ["icon_url"] = logo or Config.logo,
+                    ["name"] = (not name or name == "") and Config.webhookName or name,
+                    ["icon_url"] = (not logo or logo == "") and Config.webhookLogo or logo,
                 },
                 ["title"] = title,
                 ["description"] = description,
                 ["footer"] = {
                     ["text"] = "VORPcore" .. " • " .. os.date("%x %X %p"),
-                    ["icon_url"] = footerlogo or Config.footerLogo,
+                    ["icon_url"] = (not footerlogo or footerlogo == "") and Config.webhookFooter or footerlogo,
 
                 },
             },
