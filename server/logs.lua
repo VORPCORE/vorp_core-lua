4b7f859f0cf0da2e62cd927384cc8eb586e8eee7
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-14 18:54:41 +0000

    refactor: webhooks

--- server/logs.lua
+++ server/logs.lua
@@ -3,26 +3,36 @@
 
 RegisterServerEvent('vorp_core:addWebhook')
 AddEventHandler('vorp_core:addWebhook', function(title, webhook, description, color, name, logo, footerlogo, avatar)
+    if not webhook or webhook == "" then
+        print("^1[ERROR] ^7No webhook provided")
+        return
+    end
+    local webhookColor = (not color or color == "") and Config.webhookColor or color
+    local webhookName = (not name or name == "") and Config.name or name
+    local webhookLogo = (not logo or logo == "") and Config.logo or logo
+    local webhookFooterLogo = (not footerlogo or footerlogo == "") and Config.footerLogo or footerlogo
+    local webhookAvatar = (not avatar or avatar == "") and Config.Avatar or avatar
+
     PerformHttpRequest(webhook, function(err, text, headers)
+
     end, 'POST', json.encode({
         embeds = {
             {
-                ["color"] = (not color or color == "") and Config.webhookColor or color,
-                ["author"] = {
-                    ["name"] = (not name or name == "") and Config.name or name,
-                    ["icon_url"] = (not logo or logo == "") and Config.logo or logo,
+                color = webhookColor,
+                author = {
+                    name = webhookName,
+                    icon_url = webhookLogo,
                 },
-                ["title"] = title,
-                ["description"] = description,
-                ["footer"] = {
-                    ["text"] = "VORPcore" .. " • " .. os.date("%x %X %p"),
-                    ["icon_url"] = (not footerlogo or footerlogo == "") and Config.footerLogo or footerlogo,
+                title = title,
+                description = description,
+                footer = {
+                    text = "VORPcore" .. " • " .. os.date("%x %X %p"),
+                    icon_url = webhookFooterLogo,
 
                 },
             },
-
         },
-        avatar_url = (not avatar or avatar=="") or Config.Avatar
+        avatar_url = webhookAvatar
     }), {
         ['Content-Type'] = 'application/json'
     })
