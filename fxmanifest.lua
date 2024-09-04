38bd4428c4d0e7d9b7ff02ca5377e2daeebe794d
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-09-04 23:19:04 +0100

    new version 2.8

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
 lua54 'yes'
+
 name 'vorp core'
 description 'A Tool to build your RedM server and scripts'
 author 'VORP' -- converted from the original C# vorp core by goncalobsccosta#9041
@@ -35,7 +35,7 @@ files {
 
 ui_page 'ui/index.html'
 
-version '2.7'
+version '2.8'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/VORPCORE/vorp-core-lua'
