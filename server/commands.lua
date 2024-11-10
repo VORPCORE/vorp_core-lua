c55a9ac8cded8530ce228135e742720600f59470
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-11-10 11:57:52 +0000

    Chore(server): remove command myHours
    This doesn't exist anymore

--- server/commands.lua
+++ server/commands.lua
@@ -389,25 +389,25 @@ function MyJob(data)
 end
 
 --MY HOURS
-function MyHours(data)
-    local _source = data.source
-    local User    = CoreFunctions.getUser(_source).getUsedCharacter
-    local hours   = User.hours
+--function MyHours(data)
+--    local _source = data.source
+--    local User    = CoreFunctions.getUser(_source).getUsedCharacter
+--    local hours   = User.hours
 
-    local function isInteger(num)
-        if math.floor(num) == num then
-            return true
-        end
-        return false
-    end
+--    local function isInteger(num)
+--       if math.floor(num) == num then
+--            return true
+--        end
+--        return false
+--    end
 
-    if isInteger(hours) then
-        CoreFunctions.NotifyRightTip(_source, string.format(T.charhours, hours), 4000)
-    else
-        local newhour = math.floor(hours - 0.5)
-        CoreFunctions.NotifyRightTip(_source, string.format(T.playhours, newhour, 30), 4000)
-    end
-end
+--    if isInteger(hours) then
+--        CoreFunctions.NotifyRightTip(_source, string.format(T.charhours, hours), 4000)
+--    else
+--        local newhour = math.floor(hours - 0.5)
+--        CoreFunctions.NotifyRightTip(_source, string.format(T.playhours, newhour, 30), 4000)
+--    end
+--end
 
 function SetExp(data)
     local target = tonumber(data.args[1])
