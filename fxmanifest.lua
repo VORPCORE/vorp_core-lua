ec479e8a6b7d8ef58d216eb841de65ffd481a68e
Outsider <87246847+outsider31000@users.noreply.github.com>
2025-04-23 15:19:25 +0100

    new version 2.9

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -35,7 +35,7 @@ files {
 
 ui_page 'ui/index.html'
 
-version '2.8'
+version '2.9'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/VORPCORE/vorp_core-lua'
